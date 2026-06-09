cd33b9553fcb22284db7f9f3252045ec59d4a939
Carmen Moreno <carmen.moreno.udaondo@gmail.com>
2026-06-09 20:47:26 +0200

    fix: update lucide-react version and add RexusBexusSection to Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import { Layout } from "@/components/Layout";
 import { HeroSection } from "@/components/HeroSection";
 import { MissionSection } from "@/components/MissionSection";
 import { OutcomesSection } from "@/components/OutcomesSection";
+import { RexusBexusSection } from "@/components/RexusBexusSection";
 
 const Home = () => {
     return (
         <Layout>
             <HeroSection />
             <MissionSection />
+            <RexusBexusSection />
             <OutcomesSection />
         </Layout>
     );
